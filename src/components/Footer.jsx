135a950d2a7a98d6a03f8aa464930f7e2ae55795
xaviertessier <xaviertessier023@gmail.com>
2023-12-03 00:56:04 -0500

    footer overhaul

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,62 @@
 import '../css/Footer.css';
+import { TfiEmail } from "react-icons/tfi";
+import { FaPhone } from "react-icons/fa6";
+import { FaRegFilePdf } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className='footer-nav footer-div'>
-                <p className='footer-identification'>Lien de l'application</p>
-                <ul className='footer-nav-list'>
-                    <li>
-                        <Link to="/accueil" className="link">Accueil</Link>
-                    </li>
-                    <li>
-                        <Link to="/projets" className="link">Projets</Link>
-                    </li>
-                    <li>
-                        <Link to="/propos" className="link">À propos</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact" className="link">Contact</Link>
-                    </li>
-                </ul>
+            <div className="footer-content">
+                <div className="importantInfo footer-div">
+                    <p className='name'>Xavier Tessier</p>
+                    <div className="contact-info">
+                        <div className="email info">
+                            <TfiEmail className='icon' />
+                            <p>xaviertessier023@gmail.com</p>
+                        </div>
+                        <div className="phone info">
+                            <FaPhone className='icon' />
+                            <p>450 508-5121</p>
+                        </div>
+                    </div>
+                    <div className="footer-nav">
+                        <ul className='footer-nav-list'>
+                            <li>
+                                <Link to="/accueil" className="link">Accueil</Link>
+                            </li>
+                            <li>
+                                <Link to="/projets" className="link">Projets</Link>
+                            </li>
+                            <li>
+                                <Link to="/propos" className="link">À propos</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact" className="link">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="footer-cv footer-div">
+                    <p>Vous voulez voir mon CV ? Voici un lien pour le télécharger</p>
+                    <FaRegFilePdf className='icon'/>
+                    <Link to="pdf/xavierTessierCv.pdf" target='_blank' download={true} className='link'>Mon cv</Link> 
+                </div>
             </div>
-            <div className='separator'></div>
-            <div className="footer-presence footer-div">
-                <p className='footer-identification'>Présence sur les réseaux sociaux</p>
-                <ul className='footer-nav-list social-list'>
-                    <li>
-                        <Link to="https://www.linkedin.com/in/xavier-tessier-/" target='_blank' ><img src="logos/LI-In-Bug.png" alt="" className='linkedin' /></Link>
-                    </li>
-                    <li>
-                        <Link to="https://github.com/XavierTessier1" target='_blank' ><img src="logos/github-mark.svg" alt="" className='github' /></Link>
-                    </li>
-                </ul>
-            </div>
-            <div className='separator'></div>
-            <div className='footer-cv footer-div'>
-                <p className='footer-identification'>Mon CV</p>
-
-            </div>
+                <div className="black-bar">
+                    <p>© 2023 Xavier Tessier</p>
+                    <div className="social-list">
+                        <ul className='footer-sec-list'>
+                            <li>
+                                <Link to="https://github.com/XavierTessier1" target='_blank' ><img src="logos/github-mark.svg" alt="" className='github' /></Link>
+                            </li>
+                            <li>
+                                <Link to="https://www.linkedin.com/in/xavier-tessier-/" target='_blank' ><img src="logos/LI-In-Bug.png" alt="" className='linkedin' /></Link>
+                            </li>
+                        </ul>
+                        <p className='footer-social'>Mes réseaux sociaux</p>
+                    </div>
+                </div>
         </footer>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,42 @@
+import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../css/Header.css";
 
 const Header = () => {
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date().toLocaleTimeString());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
     const {pathname} = useLocation();
 
     return(
         <header className="header">
-            <nav className="nav">
-                <ul className="primary-nav">
-                    <li className={pathname.startsWith('/accueil') ? 'active' : null}>
-                        <Link to="/accueil" className="link">Accueil</Link>
-                    </li>
-                    <li className={pathname.startsWith('/projets') ? 'active' : null}>
-                        <Link to="/projets" className="link">Projets</Link>
-                    </li>
-                    <li className={pathname.startsWith('/propos') ? 'active' : null}>
-                        <Link to="/propos" className="link">À propos</Link>
-                    </li>
-                    <li className={pathname.startsWith('/contact') ? 'active' : null}>
-                        <Link to="/contact" className="link">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
+            <div className="header-content">
+                <nav className="nav">
+                    <ul className="primary-nav">
+                        <li className={pathname.startsWith('/accueil') ? 'active' : null}>
+                            <Link to="/accueil" className="link">Accueil</Link>
+                        </li>
+                        <li className={pathname.startsWith('/projets') ? 'active' : null}>
+                            <Link to="/projets" className="link">Projets</Link>
+                        </li>
+                        <li className={pathname.startsWith('/propos') ? 'active' : null}>
+                            <Link to="/propos" className="link">À propos</Link>
+                        </li>
+                        <li className={pathname.startsWith('/contact') ? 'active' : null}>
+                            <Link to="/contact" className="link">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="header-clock">
+                    <p className="header-clock-time">{time}</p>
+                </div>
+            </div>
         </header>
     )
 }
